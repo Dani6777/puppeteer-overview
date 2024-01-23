167502a0f8c45e5520aaaf0b0385ae4d3c0226ab
Daniel Morales <daniel.morales23@outlook.cl>
2024-01-23 00:34:38 -0300

    ADD web scraping

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import puppeteer from "puppeteer";
+import fs from 'fs/promises';
 
 
  async function openWebPage(){
@@ -16,8 +17,6 @@ import puppeteer from "puppeteer";
 }
 
 // openWebPage()
-
-
 async function captureScreenshot(){
 
     const browser = await puppeteer.launch({
@@ -31,7 +30,6 @@ async function captureScreenshot(){
     await browser.close()
 }
  //captureScreenshot()
-
  async function navigate(){
 
     const browser = await puppeteer.launch({
@@ -46,5 +44,66 @@ async function captureScreenshot(){
     await page.screenshot({path:'login.png'})
     await browser.close()
 }
+//navigate()
 
-navigate()
+
+async function getDataFromWebPage() {
+
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 500
+    });
+
+    const page = await browser.newPage();
+
+    await page.goto('https://example.com');
+
+    const result = await page.evaluate(() => {
+       const tittle = document.querySelector('h1').innerText
+       const description = document.querySelector('p').innerText
+       const more = document.querySelector('a').innerText
+       return{
+        tittle,
+        description,
+        more
+       }
+    });
+    console.log(result);
+    await browser.close();
+}
+//getDataFromWebPage();
+
+
+async function handleDynamicWebPage() {
+
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 500
+    });
+
+    const page = await browser.newPage();
+
+    await page.goto('https://quotes.toscrape.com');
+
+    const result = await page.evaluate(() => {
+
+       const quotes = document.querySelectorAll('.quote')
+
+       const data =  [...quotes].map(quote =>{
+
+        const quoteText = quote.querySelector('.text').innerText
+        const author = quote.querySelector('.author').innerText
+        const tags = [...quote.querySelectorAll('.tag')].map(tag => tag.innerText);
+        return{
+            quoteText,
+            author,
+            tags
+        }
+         }) ;
+         return data
+    });
+    console.log(result);
+    await fs.writeFile('quotes.json', JSON.stringify(result, null, 2))
+    await browser.close();
+}
+handleDynamicWebPage()
